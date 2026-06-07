77163a29d00cda14e081d579cec506594802395c
yct_notebook <yu_chengtong@163.com>
2026-06-08 03:20:57 +0800

    workbench5

---
+++ mainWidget/mainWidget/module/C3/C3.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+title 【保持打开】关闭本窗口 = 自动关闭ANSYS Workbench + Fluent
+
+::=====================================================================
+::                        客户可修改参数区
+::=====================================================================
+set "WB_PATH=C:\work\ANSYS Inc\v222\Framework\bin\Win64\runwb2.exe"
+set "SCRIPT_PATH=C:\work\Test\C3\C3.wbjn"
+echo.
+echo ======================================================
+echo           ANSYS Workbench 2022R2 自动启动
+echo ======================================================
+echo  脚本路径：%SCRIPT_PATH%
+echo.
+"%WB_PATH%" -I -R "%SCRIPT_PATH%"
